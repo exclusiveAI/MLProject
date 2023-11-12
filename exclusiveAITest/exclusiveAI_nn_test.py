b9f6625b71a225278d829ba9050793b9b1d47535
PaulMagos <p.magos@studenti.unipi.com>
2023-11-12 13:22:55 +0100

    fix on import

--- exclusiveAITest/exclusiveAI_nn_test.py
+++ exclusiveAITest/exclusiveAI_nn_test.py
@@ -1,6 +1,6 @@
-import exclusiveAI
+from exclusiveAI import neural_network
 
 def test_nn():
-    assert exclusiveAI.neural_network(
+    assert neural_network.rectagle_area(
         4.895168, 52.370216
     ) == 256.361005516
