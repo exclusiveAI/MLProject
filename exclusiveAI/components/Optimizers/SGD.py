3c8ae5c1efeea4faead298dac6811041b2de487b
PaulMagos <p.magos@studenti.unipi.com>
2023-11-22 00:39:16 +0100

    Optimizers operation optimization

--- exclusiveAI/components/Optimizers/SGD.py
+++ exclusiveAI/components/Optimizers/SGD.py
@@ -10,12 +10,11 @@ class SGD(Optimizer):
     #     Gradient Descent algorithm
     def update(self, model, y_true, x):
         if not self.old_dw: self.old_dw = [0 for _ in model.layers]
-        deltas = self.calulate_deltas(model, y_true, x)
+        dw = self.calulate_deltas(model, y_true, x)
 
-        new_deltas = []
-        for layer, delta, old_delta in myzip(model.layers, deltas, self.old_dw):
-            new_delta = self.learning_rate * delta + self.momentum * old_delta
-            new_deltas.append(new_delta)
+        new_deltas = self.learning_rate * dw + self.momentum * self.old_dw
+        
+        for layer, new_delta in myzip(model.layers, new_deltas):
             layer.weights = layer.weights + new_delta - layer.weights * self.regularization * 2
 
-        self.old_dw = new_deltas if sum(self.old_dw)==0 else deltas
+        self.old_dw = new_deltas if sum(self.old_dw)==0 else dw
