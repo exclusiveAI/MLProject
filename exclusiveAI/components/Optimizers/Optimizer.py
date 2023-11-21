4d6ab6af7920a7d7e199d5b37bc318721fa229ad
PaulMagos <p.magos@studenti.unipi.com>
2023-11-22 00:10:18 +0100

    Optimizers, fi

--- exclusiveAI/components/Optimizers/Optimizer.py
+++ exclusiveAI/components/Optimizers/Optimizer.py
@@ -1,12 +1,12 @@
 __all__ = ['Optimizer']
+from exclusiveAI.components.Schedulers import LearningRateScheduler
 from exclusiveAI.components.ActivationFunctions import ActivationFunction
-from exclusiveAI.components.Layers import Layer
 class Optimizer:
-    def __init__(self, learning_rate: float, regularization: float, activation_func: ActivationFunction, layer: Layer):
+    def __init__(self, learning_rate: float, regularization: float, activation_func: ActivationFunction, lr_scheduler: LearningRateScheduler):
         self.learning_rate = learning_rate
         self.regularization = regularization
         self.activation_function = activation_func
-        self.layer = layer
+        self.lr_scheduler = lr_scheduler
         self.old_dw = []
 
     def calulate_deltas(self, model, y_true, x):
@@ -19,3 +19,6 @@ class Optimizer:
             # Pass y_true
             deltas.insert(0, layer.backpropagate(y_true))
         return deltas      
+    
+    def update_lr(self):
+        self.learning_rate = self.lr_scheduler.update(self.learning_rate)
