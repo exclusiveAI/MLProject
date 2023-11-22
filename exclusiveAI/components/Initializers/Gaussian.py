1e8345ff7ee8d9f415580971526776c1340c0e5b
PaulMagos <p.magos@studenti.unipi.com>
2023-11-22 11:31:46 +0100

    Initializers done

--- exclusiveAI/components/Initializers/Gaussian.py
+++ exclusiveAI/components/Initializers/Gaussian.py
@@ -1,1 +1,12 @@
+from .Initializer import Initializer
+import numpy as np
+__all__ = ['Gaussian']
 
+class Gaussian(Initializer):
+    def __init__(self, mean=0, std=0.05):
+        super().__init__(name = 'Gaussian')
+        self.mean = mean
+        self.std = std
+
+    def initialize(self, shape):
+        return np.random.normal(self.mean, self.std, shape)
