7dfc8f6553624b55bc214977e401295737e565f0
Francesco Paolo Liuzzi <f.liuzzi@studenti.unipi.it>
2023-11-22 15:07:30 +0100

    add layers

--- exclusiveAI/components/Layers/Layer.py
+++ exclusiveAI/components/Layers/Layer.py
@@ -2,10 +2,12 @@ __all__ = ['Layer']
 
 from exclusiveAI.components.ActivationFunctions import ActivationFunction
 from exclusiveAI.components.Initializers import Initializer
+import numpy as np
+
 
 
 class Layer:
-    def __init__(self, units: int, inizializer: Initializer, activation_func: ActivationFunction, is_trainable=False):
+    def __init__(self, units: int, inizializer: Initializer, activation_func: ActivationFunction, is_trainable=True):
         # Layers
         self.next: Layer = None
         self.prev: Layer = None
@@ -30,7 +32,18 @@ class Layer:
     def feedforward(self, input):
         if not self.is_initialized:
             raise Exception("Layer not initialized")
-
+        
+        input = np.insert(input, 0, 1, axis=-1)     # adding bias to input
+        
         self.nets = self.weights @ input
         self.output = self.activation_func.function(self.nets)
         return self.output
+    
+    def backpropagate(self):
+        if not self.is_initialized:
+            raise Exception("Layer not initialized")
+        
+        # calculate the product between the error signal and incoming weifhts from current unit
+        self.error = self.next.error @ self.next.weights[1:, :].T
+        self.error = self.activation_func.derivative(self.nets) * self.error
+        return np.dot(self.prev.output.T, self.error)
