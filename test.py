8da63f85984c1d057efd494c65a62fb56cab2e48
PaulMagos <p.magos@studenti.unipi.com>
2023-12-04 23:33:21 +0100

    fixOptimizers, adding adam and merging nesterov

--- test.py
+++ test.py
@@ -62,29 +62,29 @@ train = one_hot_encoding(train)
 test = np.array(test.values.tolist())
 test = one_hot_encoding(test)
 
-ea = EarlyStoppingCallback(patience_limit=10)
-values = [0.1, 0.01, 0.05, 0.5]
+ea = EarlyStoppingCallback(patience_limit=100)
+values = [0.001, 0.0001]
 values2 = [0.0001, 0.001]
 
-uniform = Uniform(low=-0.5, high=0.5)
+uniform = Uniform(low=-1, high=1)
 
 myconfigurator = ConfiguratorGen(random=False, regularizations=values2, learning_rates=values,
                                  loss_functions=['mse'], optimizer=['sgd'],
                                  activation_functions=['sigmoid'],
-                                 number_of_units=[10], number_of_layers=[1, 2],
-                                 momentums=[0.9], initializers=[uniform], input_shapes=train.shape, verbose=False,
+                                 number_of_units=[10], number_of_layers=[1, 2, 3],
+                                 momentums=[0.9, 0.5, 0.1, 0.01], initializers=[uniform], input_shapes=train.shape, verbose=False,
                                  callbacks=[ea], output_activation='sigmoid'
                                  )
 
 myval = HoldOut(models=myconfigurator, input=train, target=train_label, debug=True)
 config = myval.hold_out()
 
-ea = EarlyStoppingCallback(patience_limit=10)
+ea = EarlyStoppingCallback(patience_limit=100)
 config['callbacks'] = [ea, 'wandb']
 model = Composer(config=config).compose()
 
 print("Model found:", config)
-model.train(train, train_label, test, test_label, epochs=100)
+model.train(train, train_label, test, test_label, epochs=1000)
 
 res = model.evaluate(input=test, input_label=test_label)
 
