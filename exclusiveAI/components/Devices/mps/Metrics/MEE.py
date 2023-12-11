e8b193edf12909ed8f25afa1a88e30ebca35725d
PaulMagos <p.magos@studenti.unipi.com>
2023-12-11 09:49:41 +0100

    Update to mps

--- exclusiveAI/components/Devices/mps/Metrics/MEE.py
+++ exclusiveAI/components/Devices/mps/Metrics/MEE.py
@@ -1,5 +1,6 @@
 from .Metric import Metric
 import numpy as np
+import mlx.core as mps
 
 __all__ = ['MEE']
 
@@ -10,5 +11,5 @@ class MEE(Metric):
     """
     def __init__(self):
         super().__init__(name='mee',
-                         f=lambda y_pred, y_true: np.mean((np.linalg.norm(y_true - y_pred, axis=1))),
+                         f=lambda y_pred, y_true: mps.mean(mps.sqrt(mps.sum(mps.square(y_pred - y_true), axis=1))),
                          )
--- exclusiveAI/components/Devices/mps/Metrics/MSE.py
+++ exclusiveAI/components/Devices/mps/Metrics/MSE.py
@@ -1,5 +1,6 @@
 from .Metric import Metric
 import numpy as np
+import mlx.core as mps
 
 __all__ = ['MSE']
 
@@ -9,5 +10,5 @@ class MSE(Metric):
     Mean squared error (MSE)
     """
     def __init__(self):
-        f = lambda y_pred, y_true: np.mean(np.sum(np.square(y_pred - y_true), axis=1))
+        f = lambda y_pred, y_true: mps.mean(mps.sum(mps.square(y_pred - y_true), axis=1))
         super().__init__(name='mse', f=f)
