2e81b3a7f1517f745fd5c06e23b6e8e1b09b5b4b
PaulMagos <p.magos@studenti.unipi.com>
2024-01-08 05:12:09 +0100

    Fix perche lo vuole pycharm

--- TEST_TF.py
+++ TEST_TF.py
@@ -17,13 +17,13 @@ test_idx = np.array(data_dict['test_idx'])
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(units=20, activation='sigmoid'),
-    tf.keras.layers.Dense(units=15, activation='sigmoid'),
-    tf.keras.layers.Dense(units=10, activation='sigmoid'),
+    tf.keras.layers.Dense(units=20, activation='sigmoid'),
     tf.keras.layers.Dense(units=3, activation='linear')
 ])
 
-model.compile(optimizer=SGD(learning_rate=0.005, nesterov=True, momentum=0, weight_decay=1e-8), loss='mean_squared_error', metrics=['mean_absolute_error'])
+model.compile(optimizer=SGD(learning_rate=0.005, nesterov=False, momentum=0.1, weight_decay=1e-8), loss='mean_squared_error', metrics=['mean_absolute_error'])
 model.fit(training_data, training_labels, epochs=1000, batch_size=200)
-model.evaluate(test_data, test_labels)
+res = model.evaluate(test_data, test_labels)
 model.save('model.h5')
 prediction = model.predict(test_data)
+print(res)
